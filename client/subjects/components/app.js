1627eaeaaa8c89179b297c32f2d382371363791e
Marco <marco.calca@gmail.com>
2016-06-07 16:24:36 +0000

    subjects init not working

--- client/subjects/components/app.js
+++ client/subjects/components/app.js
@@ -1,6 +1,18 @@
-angular.module('artoo', [
-  'appbar',
-])
-  .config((AppbarSrvProvider) => {
-    AppbarSrvProvider.setSubject({name: 'Components', url: 'subjects/components'});
+//define app module
+var app = angular
+  .module('artooInit', [])
+  .run(function(){
+    console.info("Run from setter");
   });
+  
+angular
+  .module('artooInit')
+  //.config(angular.noop)
+  .run(() => {
+    console.info('Run from getter!');
+  });
+
+app.run(function(){
+  console.info("Run from a var");
+})
+//define run function
