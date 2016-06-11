79c5b775018ab3e3da82a5ab15d391a1e8050c1a
Marco <marco.calca@gmail.com>
2016-06-11 10:28:22 +0000

    feature(): Transport.get() - Setting up complete

---
+++ client/project/transport/transport.controller.js
@@ -0,0 +1,5 @@
+angular.module('tm').controller('TransportCtrl', function($scope, TransportSrv){
+   console.log("aa")
+   $scope.TransportSrv = TransportSrv;
+
+});
--- client/project/transport/transport.service.js
+++ client/project/transport/transport.service.js
@@ -1,17 +1,18 @@
 angular.module('tm').service('TransportSrv', function(){
    var transports = [
       {
-         id: '',
-         customer: '',
-         placeLoad: '',
-         placeUnload: '',
-         dateLoad: '',
-         dateUnload: '',
-         weigth: '',
+         id: '01',
+         customer: 'Company A',
+         placeLoad: 'Montecassiano',
+         placeUnload: 'Ancona',
+         dateLoad: '23/05/2016',
+         dateUnload: '25/05/2016',
+         weigth: '50',
       },
    ];
 
    this.get = () => {
+      console.info("loaded" + transports);
       return transports;
    };
 });
