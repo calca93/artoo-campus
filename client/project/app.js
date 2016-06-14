5a0b6492e5e3b4822e9a79ec2a3984d590bd8a34
Marco <marco.calca@gmail.com>
2016-06-14 15:57:26 +0000

    project.feature() list material

--- client/project/app.js
+++ client/project/app.js
@@ -1,1 +1,15 @@
-angular.module('tm', []);
+// angular.module('tm', []);
+angular.module('tm', [
+   'ngMessages',
+   'ngAria',
+   'ngAnimate',
+   'ngMaterial',
+])
+   .config(($mdThemingProvider) => {
+      $mdThemingProvider
+         .theme('default')
+         .primaryPalette('blue-grey')
+         .accentPalette('blue')
+         .warnPalette('red')
+         .backgroundPalette('grey');
+   });
--- client/project/transport/transport.controller.js
+++ client/project/transport/transport.controller.js
@@ -1,6 +1,8 @@
 angular.module('tm').controller('TransportCtrl', function(TransportSrv){
    this.TransportSrv = TransportSrv;
 
+   this.selectedShowOptions = 'In progress';
+
    this.showOptions = [{
          prop: 'all',
          title: 'All',
@@ -11,4 +13,10 @@ angular.module('tm').controller('TransportCtrl', function(TransportSrv){
          prop: 'archived',
          title: 'Archived',
       },];
+
+   this.minDate = new Date(2016, 5, 17);
+   this.filterPredicate = function(date) {
+      var day = date.getDay();
+      return day !== 0 && day !== 6;
+   };
 });
